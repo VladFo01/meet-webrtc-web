603fcf065ff04b41c62b68a3813d3f358670713d
davidbogomolov <david.bogomolov2@gmail.com>
2023-05-07 17:18:48 +0300

    added icons to Room.jsx

--- src/pages/Room/Room.jsx
+++ src/pages/Room/Room.jsx
@@ -1,6 +1,10 @@
 import '../../styles/index.css';
 import '../../styles/norm.css';
 
+import { AiFillAudio } from "react-icons/ai";
+import {MdCallEnd} from "react-icons/md";
+import {BsFillCameraVideoOffFill} from "react-icons/bs";
+
 const Room =() => {
     return (
         <div className='room-wrapper default-bg'>
@@ -8,9 +12,9 @@ const Room =() => {
 
 
             <div className='footer-toggler-btns'>
-                <btn className='room-btn'>Camera off</btn>
-                <btn className='room-btn'>Mute</btn>
-                <btn className='room-btn'>Leave Room</btn>
+                <btn className='room-btn'><BsFillCameraVideoOffFill/></btn>
+                <btn className='room-btn'><AiFillAudio/></btn>
+                <btn className='room-btn'><MdCallEnd /></btn>
             </div>
         </div>
     )
